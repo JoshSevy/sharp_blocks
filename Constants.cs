3e4f747e097c1ca670abe9f5bb01479afa08dec9
JoshSevy <joshuasevy@outlook.com>
2022-05-01 17:41:45 -0600

    Fix typo with screen size

--- Constants.cs
+++ Constants.cs
@@ -15,8 +15,8 @@ namespace Blocks
         public const double SPEED_BUMP = 1.1;
 
         // Window 
-        public const int VIRTUAL_HEIGHT = 480;
-        public const int VIRTUAL_WIDTH = 640;
+        public const int VIRTUAL_HEIGHT = 640;
+        public const int VIRTUAL_WIDTH = 480;
         public const int WINDOW_HEIGHT = 1024;
         public const int WINDOW_WIDTH = 768;
     }
